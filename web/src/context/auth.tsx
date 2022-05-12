dca2e79aa34fd3bb5bfa4b7236932a3a7af78a27
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-05-12 19:25:04 -0300

    auth context

---
+++ web/src/context/auth.tsx
@@ -0,0 +1,74 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type AuthResponse = {
+    token: string, 
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+}
+
+type User = {
+    id: string;
+    name: string;
+    login: string;
+    avatar_url: string;
+}
+
+type AuthContextData = {
+    user: User | null;
+    signInUrl: string;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+type AuthProvider = {
+    children: ReactNode;
+}
+
+export function AuthProvider(props: AuthProvider) {
+    const [user, setUser] = useState<User | null>(null)
+
+    const signInUrl = `https://github.com/login/oauth/authorize?scop=user&client_id=dd70a54ee5923543ca02`
+
+    async function signIn(githubCode: string) {
+        const response = await api.post<AuthResponse>('authenticate', {
+            code: githubCode,
+        })
+
+        const { token, user } = response.data
+
+        // armazenando token no navegador
+        localStorage.setItem('@dowhile:token', token)
+
+        setUser(user)
+    }
+
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes("?code=")
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=')
+
+            // limpar o code da url
+            window.history.pushState({}, '', urlWithoutCode)
+
+            signIn(githubCode);
+        }
+    }, [])
+
+    return (
+        // todos os componentes que precisar de autenticação, vai vir daqui
+        <AuthContext.Provider value={{ signInUrl, user }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+/* children é o que incluimos dentro do componente
+   nesse caso todos os componentes vao ter acesso
+*/
